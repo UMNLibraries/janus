8ec4f9c7d2fb8764b933d8ba0fab149e2b58dcdb
David Naughton <nihiliad@gmail.com>
2016-08-10 15:40:14 -0500

    First commit with session implementation.

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ const favicon = require('koa-favicon');
 const router = require('koa-router')();
 const co = require('bluebird').coroutine;
 const bunyan = require('bunyan');
+const uuid = require('uuid');
+const session = require('koa-session');
+const convert = require('koa-convert');
 const InvalidArgumentError = require(path.resolve(__dirname, 'invalid-arg-error'));
 
 module.exports = stampit()
 .props({
   uriFactoryPlugins: {},
+  sessionOpts: {
+    key: 'janus:sess',
+    signed: false
+  },
   redirectLog: {name: 'redirect'},
   errorLog: {name: 'error'},
   uriPathPrefix: '/',
   favicon: path.resolve(__dirname, 'assets/favicon.ico'),
 })
 .methods({
+  sessionId (ctx) {
+    return new Promise(function (resolve, reject) {
+      resolve(uuid.v1());
+    });
+  }, 
+
   errorLogger () {
     return bunyan.createLogger(this.errorLog);
   },
@@ -25,25 +38,35 @@ module.exports = stampit()
     return bunyan.createLogger(this.redirectLog);
   },
 
-  redirectLogEvent (ctx) {
+  redirectLogEvent (ctx, defaultEvent) {
+    return defaultEvent;
+  },
+
+  defaultRedirectLogEvent (ctx) {
     return {
       'request': {
-        'method': ctx.req.method,
-        'url': ctx.req.url,
-        'referer': ctx.req.headers['referer'],
-        'userAgent': ctx.req.headers['user-agent'],
+        'method': ctx.request.method,
+        'url': ctx.request.url,
+        'referer': ctx.request.header['referer'],
+        'userAgent': ctx.request.header['user-agent'],
+      },
+      'response': {
+        'location': ctx.response.header['location'],
       },
       'target': ctx.request.query.target,
       'search': ctx.request.query.search,
       'scope': ctx.request.query.scope,
       'field': ctx.request.query.field,
+      'sessionId': ctx.session.id,
     };
   },
 })
 .init(function () {
   const factory = require(path.resolve(__dirname, 'uri-factory/'))(this.uriFactoryPlugins);
+  const sessionId = this.sessionId;
   const redirectLogger = this.redirectLogger();
   const errorLogger = this.errorLogger();
+  const defaultRedirectLogEvent = this.defaultRedirectLogEvent;
   const redirectLogEvent = this.redirectLogEvent;
 
   router.get(this.uriPathPrefix, co(function *redirect (ctx, next) {
@@ -52,15 +75,20 @@ module.exports = stampit()
     ctx.status = 302;
     ctx.redirect(uri.href());
     if (warning) {
-      redirectLogger.warn({'event': redirectLogEvent(ctx)}, warning);
+      redirectLogger.warn({'event': redirectLogEvent(ctx, defaultRedirectLogEvent(ctx))}, warning);
     } else {
-      redirectLogger.info({'event': redirectLogEvent(ctx)}, 'ok');
+      redirectLogger.info({'event': redirectLogEvent(ctx, defaultRedirectLogEvent(ctx))}, 'ok');
     }
   }));
 
   const app = new Koa();
   app
     .use(favicon(this.favicon))
+    .use(convert(session(this.sessionOpts, app)))
+    .use(co(function *session (ctx, next) {
+      ctx.session.id = yield sessionId(ctx);
+      yield next();
+    }))
     .use(router.routes())
     .use(router.allowedMethods())
     .on('error', (err, ctx) => {
