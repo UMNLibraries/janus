07075287696e8b06671c1a55305a04598862b8a2
John Barneson <barne102@umn.edu>
2025-03-21 10:34:28 -0500

    Trying to fix pluginFactory is not a function error

--- uri-factory/index.js
+++ uri-factory/index.js
@@ -36,7 +36,7 @@ export default stampit()
   }).init(function (plugins) {
     const factory = this
     for (const [pluginName, pluginFactory] of Object.entries(plugins)) {
-      const plugin = pluginFactory()
+      const plugin = pluginFactory
       if (!(Reflect.has(plugin, 'uriFor') && (Reflect.getPrototypeOf(plugin.uriFor) === Function.prototype))) {
         throw new InvalidArgumentError(`plugin "${pluginName}" has no uriFor() method`)
       }
